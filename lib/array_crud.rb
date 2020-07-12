4eaa0e834146a3c5285c3e62fda9e92fc765ff9d
Courtney Yargo <courtneyyargo@gmail.com>
2020-07-12 19:30:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,7 +26,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   numbers = ["1", "2", "3", "4"]
-  numbers.index = ["1"]
+  numbers.index["1"]
 end
 
 def retrieve_first_element_from_array(array)
