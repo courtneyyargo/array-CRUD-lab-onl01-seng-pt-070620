d17b45bef4d2b7eb7a158fb351868ae3fc2a5413
Courtney Yargo <courtneyyargo@gmail.com>
2020-07-12 19:17:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,10 +19,6 @@ end
 def remove_element_from_end_of_array(array)
   numbers = ["1", "2", "3", "4"]
   arrays!(remove_element_from_end_of_array(array)) = numbers.pop
-  
-  
-    array = ["I", "am", "really", "learning", "arrays!"]
-    expect(remove_element_from_end_of_array(array)).to eq("arrays!")
 end
 
 def remove_element_from_start_of_array(array)
